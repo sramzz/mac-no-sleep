c055d99c8b14078e1653190ac8b2adee62d0d3cf
Santiago Ramirez <sramzz@outlook.com>
2026-09-15 14:05:30 +0200

    feat(app): configure LSUIElement and accessory activation policy to hide from Dock

--- Sources/PreventSleepApp/main.swift
+++ Sources/PreventSleepApp/main.swift
@@ -57,6 +57,7 @@ if CommandLine.arguments.contains("--register") {
 }
 
 let app = NSApplication.shared
+app.setActivationPolicy(.accessory)
 let delegate = AppDelegate()
 app.delegate = delegate
 app.run()
